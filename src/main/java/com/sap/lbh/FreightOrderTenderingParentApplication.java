ead3237526001d8a053c43a0e33d1256194c5584
Chandrashekar <I033516@global.corp.sap>
2016-12-09 10:39:21 +0530

    First Commit

---
+++ src/main/java/com/sap/lbh/FreightOrderTenderingParentApplication.java
@@ -0,0 +1,12 @@
+package com.sap.lbh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreightOrderTenderingParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreightOrderTenderingParentApplication.class, args);
+	}
+}
